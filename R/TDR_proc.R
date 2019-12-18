48e94cb7423f147c39308233cdde1010671b62c1
Kraska <KRASKAPE@dfo-mpo.ca>
2019-12-18 16:11:53 -0400

    Initial commit

---
+++ R/TDR_proc.R
@@ -0,0 +1,71 @@
+#' @title SABS wharf long term temperature logging data transformation function
+#'
+#' @author Peter Kraska, \email{peter.kraska@dfo-mpo.gc.ca}
+#'
+#' @
+#' @description function to transform Hobo temperature logger data that has been
+#'   converted from proprietary binary format to a comma separated text file
+#'   into a format that can be ingested in to the COERS.TDRLOG Oracle database
+#'   tables on the DFO production database (PTRAN).
+#'
+#' @param x a comma delimited text file created from the Hobo temperature logger
+#'   manufacturer software with the following columns `#`, `Date Time,
+#'   GMT+00:00`, `Abs Pres, psi (LGR S/N: xxxx, SEN S/N: xxxx)`, `Temp, °C (LGR
+#'   S/N: xxxx, SEN S/N: xxxx)`, `Water Level, meters (LGR S/N: xxxx)`
+#'
+#' @return returns a comma delimited text file in the format
+#'   `[YEAR]-[MONTH]-[DAY]__SABS_warf_water_temperature_data.csv` for importing
+#'   into COERS.TDRLOG
+#'
+#' @export
+#'
+#' @examples tdr_proc('hobo_data_file.csv')
+#' @importFrom rlang .data
+
+tdr_proc <- function(x) {
+  requires(tidyr)
+  requires(lubridate)
+
+  raw <-
+    read_csv(x, skip = 1)
+
+  raw %>%
+    rename_all( ~ c("id", "datet", "press", "temp", "depth")) %>%
+    mutate(
+      datet = mdy_hms(.data$datet),
+      SDATE = paste0(
+        day(.data$datet),
+        "-",
+        month(.data$datet, label = TRUE, abbr = TRUE),
+        "-",
+        format(.data$datet, "%y")
+      ),
+      YEAR = year(.data$datet),
+      MONTH = month(.data$datet),
+      DAY = day(.data$datet),
+      HOUR = hour(.data$datet),
+      MINUTE = minute(.data$datet),
+      STATION = 1,
+      TEMP = .data$temp,
+      DEPTH = .data$press
+    ) %>%
+    select(
+      .data$STATION,
+      .data$SDATE,
+      .data$YEAR,
+      .data$MONTH,
+      .data$DAY,
+      .data$HOUR,
+      .data$MINUTE,
+      .data$TEMP,
+      .data$DEPTH
+    ) %>%
+    write_csv(paste0(
+      last(.data$YEAR),
+      "-",
+      str_pad(last(.data$MONTH), 2, "left", "0"),
+      "-",
+      str_pad(last(.data$DAY), 2, "left", "0"),
+      "_SABS_warf_water_temperature_data.csv"
+    ))
+}
---
+++ R/wind_proc.R
@@ -0,0 +1,76 @@
+#' SABS wharf long term wind speed and direction logging data transformation
+#' function
+#'
+#' @param x comma delimited wind speed and direction file from the SABS wharf
+#'   wind speed and direction logger
+#' @param station `STATION` to be used for identification in the
+#'   `COERS.WINDDATA` table on `PTRAN`. Default value is `1` (only value in the
+#'   table).
+#' @param gaugenum `GAUGENUM` probably wind speed and direction logger
+#'   identification number, but only `999` present in the the `COERS.WINDDATA`
+#'   table
+#' @param latitude Latitude of wind speed and direction logger unit measurements
+#'   in decimal degrees WGS84
+#' @param longitude Longitude of wind speed and direction logger unit
+#'   measurements in decimal degrees WGS84
+#' @param area Character string for location of wind speed and direction logger
+#'
+#' @return Returns a comma delimited text file in
+#'   `R:/Science/CESD/COERS/FPage/data/Wind logger/FOR ORACLE IMPORT/` for
+#'   upload to PTRAN by COERS staff.
+#' @export
+#'
+#' @examples wind_proc(x)
+#'
+
+wind_proc <-
+  function(x,
+           station = 1,
+           gaugenum = 999,
+           latitude = 45.08275,
+           longitude = 67.08508,
+           area = "SABS Wharf") {
+    read_csv(
+      x,
+      skip = 2,
+      col_names = c("date", "time", "key", "value", "unit", "x"),
+      col_types = list(
+        date = col_character(),
+        time = col_time(),
+        key = col_factor(),
+        value = col_double(),
+        unit = col_factor(),
+        x = col_factor()
+      ),
+      trim_ws = TRUE,
+      skip_empty_rows = TRUE
+    ) %>%
+      pivot_wider(names_from = c(key, unit), values_from = value) %>%
+      mutate(
+        STATION = station,
+        GAUGENUM = gaugenum,
+        SDATE = mdy_hms(paste(date, time, sep = " ")),
+        YEAR = year(SDATE),
+        MONTH = month(SDATE),
+        DAY = day(SDATE),
+        HOUR = hour(SDATE),
+        MINUTE = minute(SDATE),
+        SPEED = `WndSpd_m/s`,
+        DIRECTION = WndDir_Degrees,
+        LATITUDE = latitude,
+        LONGITUDE = longitude,
+        AREA = area
+      ) %>%
+      select(-1:-5) %>%
+      write_csv(
+        paste0(
+          "R:/Science/CESD/COERS/FPage/data/Wind logger/FOR ORACLE IMPORT/",
+          last(.$YEAR),
+          "-",
+          str_pad(last(.$MONTH), 2, "left", "0"),
+          "-",
+          str_pad(last(.$DAY), 2, "left", "0"),
+          "_SABS_wharf_wind_data.csv"
+        )
+      )
+  }
