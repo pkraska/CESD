02d7fd03e8aee4f45aad897fdc7e683c242ed18a
Peter Kraska <peterkraska@gmail.com>
2020-01-02 22:12:01 -0400

    updated ADCP_proc.R to use tidyverse almost excludively
    ftill using data.table::fread as readr::read_delim is not working well. Still much faster than before, from ~1min to ~5seconds.

--- R/adcp_proc.R
+++ R/adcp_proc.R
@@ -11,85 +11,60 @@
 #'
 #' @import tidyverse
 #' @import data.table
-#' @import dtplyr
 
 ADCP_proc <- function(x) {
-    raw <- fread(x, header = FALSE, fill = TRUE) %>%
-      as.tbl() %>%
-      mutate(
-        header = case_when(
-          V2 == "cm" ~ TRUE,
-          lead(V2, 5) == "cm" ~ TRUE,
-          lead(V2, 4) == "cm" ~ TRUE,
-          lead(V2, 3) == "cm" ~ TRUE,
-          lead(V2, 2) == "cm" ~ TRUE,
-          lead(V2, 1) == "cm" ~ TRUE,
-          TRUE ~ FALSE
-        )
-      )
-
-    data$raw <- raw
-
-    headerRaw <- raw %>%
-      filter(header == TRUE) %>%
-      mutate(ensemble = rep(c(1:(n() / 6)), each = 6))
-
-    ens2String <- function(x) {
-      toString(c(x[1,], x[2,], x[3,], x[4,], x[5,], x[6,]))
-    }
-
-    ensemble <- headerRaw %>%
-      group_by(ensemble) %>%
-      summarise(header = ens2String(.))
-
-    ensemble %>%
-      mutate(header = str_split(string = header, pattern = ","))
-
-    dim(ensemble) <- c(1, dim(ensemble)[1] * dim(ensemble)[2])
-
-    for (i in 2:max(headerRaw$ensemble)) {
-      ens <- headerRaw %>%
-        filter(ensemble == i) %>%
-        select(-ensemble, -header) %>%
-        as.matrix()
-
-      dim(ens) <- c(1, dim(ens)[1] * dim(ens)[2])
-
-      ensemble <- rbind(ensemble, ens)
-    }
-
-    data$header <- ensemble
-
-
-
-
-    cleanHeader <- header %>%
-      select(-27, -33:-40, -46:-53, -63:-66, -73:-79)
-
-    # create a tibble of run lengths, filter out header
-    rle <- rle(data$header) %>%
-      unclass() %>%
-      data.frame() %>%
-      filter(values == FALSE)
-
-    body <- data %>%
-      filter(header == FALSE) %>%
-      mutate(ensemble = rep(c(1:length(rle$lengths)), times = rle$lengths)) %>%
-      select(-header) %>%
-      select(ensemble, everything())
-
-    allData <- left_join(body, cleanHeader, by = "ensemble")
-
-    message(
-      paste0(
-        "Finished processing ",
-        file.list[i],
-        ", saving as CSV in working directory. \n \n"
+  raw <- fread(x, header = FALSE, fill = TRUE) %>%
+    as.tbl() %>%
+    mutate(
+      header = case_when(
+        V2 == "cm" ~ TRUE,
+        lead(V2, 5) == "cm" ~ TRUE,
+        lead(V2, 4) == "cm" ~ TRUE,
+        lead(V2, 3) == "cm" ~ TRUE,
+        lead(V2, 2) == "cm" ~ TRUE,
+        lead(V2, 1) == "cm" ~ TRUE,
+        TRUE ~ FALSE
       )
     )
 
-    write_csv(allData, paste(substr(file.list[i], 1, nchar(file.list[i]) - 4), ".csv", sep = ""))
-
-    progFiles$tick()$print
+  # Function to take the 6 rows identified as the header, and widen them/unstack
+  # them into a single line.
+  ens2String <- function(x) {
+    toString(c(x[1, ], x[2, ], x[3, ], x[4, ], x[5, ], x[6, ]))
   }
-}
+
+  # filter the raw data to only the header information and assign an ensemble
+  # number to the 6 rows
+  headerRaw <- raw %>%
+    filter(header == TRUE) %>%
+    mutate(ensemble = rep(c(1:(n(
+    ) / 6)), each = 6)) %>%
+    group_by(ensemble) %>%
+    do(header = ens2String(.)) %>%
+    as_tibble() %>%
+    separate(header, paste0("V", c(1:90)), sep = ",")
+
+  # Remove the columns created by the header and ensemble columns created to
+  # keep track of ensembles in headerRaw
+  header <- headerRaw %>%
+    select(-15, -16, -30, -31, -45, - 46, -60, -61, -75, -76, -90, -91)
+
+  # create a tibble of run lengths, filter out header
+  rle <- rle(raw$header) %>%
+    unclass() %>%
+    as_tibble() %>%
+    slice(2:n()) %>%
+    filter(values == FALSE)
+
+  ensemble <- raw %>%
+    slice(2:n()) %>%
+    filter(header == FALSE) %>%
+    mutate(ensemble = rep(c(1:length(rle$lengths)), times = rle$lengths)) %>%
+    select(-header) %>%
+    select(ensemble, everything()) %>%
+    mutate_at(vars(starts_with("V")), as.numeric)
+
+  list(raw = raw, header = header, data = ensemble)
+
+  }
+
