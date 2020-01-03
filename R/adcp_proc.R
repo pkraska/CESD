da9aed9ee028dac7db04af331efac89d9684a3db
Peter Kraska <peterkraska@gmail.com>
2020-01-03 08:41:30 -0400

    Update adcp_proc.R

--- R/adcp_proc.R
+++ R/adcp_proc.R
@@ -1,10 +1,11 @@
 #' Converting ADCP ensembles into a header and ensemble data tables
 #'
-#' @param x text file containing ADCP data from either a Teledyne workshorse or
-#'   Riverray units
+#' @param x text file containing ADCP data from either a Teledyne Riverray units
+#'   with a 6 line header and a single extra line at the start of the data.
 #'
-#' @return A comma delimited file with the ensemble metadata and data in a tidy
-#'   format.
+#' @return A list composed of the raw data, the flattened header information, as
+#'   well as the actual ensemble data.
+#'
 #' @export
 #'
 #' @examples ADCP_proc(x)
@@ -30,7 +31,7 @@ ADCP_proc <- function(x) {
   # Function to take the 6 rows identified as the header, and widen them/unstack
   # them into a single line.
   ens2String <- function(x) {
-    toString(c(x[1, ], x[2, ], x[3, ], x[4, ], x[5, ], x[6, ]))
+    toString(c(x[1,], x[2,], x[3,], x[4,], x[5,], x[6,]))
   }
 
   # filter the raw data to only the header information and assign an ensemble
@@ -38,6 +39,7 @@ ADCP_proc <- function(x) {
   headerRaw <- raw %>%
     filter(header == TRUE) %>%
     mutate(ensemble = rep(c(1:(n(
+
     ) / 6)), each = 6)) %>%
     group_by(ensemble) %>%
     do(header = ens2String(.)) %>%
@@ -47,7 +49,7 @@ ADCP_proc <- function(x) {
   # Remove the columns created by the header and ensemble columns created to
   # keep track of ensembles in headerRaw
   header <- headerRaw %>%
-    select(-15, -16, -30, -31, -45, - 46, -60, -61, -75, -76, -90, -91)
+    select(-15,-16,-30,-31,-45,-46,-60,-61,-75,-76,-90,-91)
 
   # create a tibble of run lengths, filter out header
   rle <- rle(raw$header) %>%
@@ -64,7 +66,8 @@ ADCP_proc <- function(x) {
     select(ensemble, everything()) %>%
     mutate_at(vars(starts_with("V")), as.numeric)
 
-  list(raw = raw, header = header, data = ensemble)
+  list(raw = raw,
+       header = header,
+       data = ensemble)
 
-  }
-
+}
