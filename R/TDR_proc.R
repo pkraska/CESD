7db323b9312d186a28a7af4b0f039e6e76fa625f
Kraska, Peter <peter.kraska@dfo-mpo.gc.ca>
2020-01-14 15:37:04 -0400

    updated the SDATE field to pad H/M/S with 0

--- R/TDR_proc.R
+++ R/TDR_proc.R
@@ -29,7 +29,7 @@ tdr_proc <- function(x) {
     read_csv(x, skip = 1)
 
   raw %>%
-    rename_all( ~ c("id", "datet", "press", "temp", "depth")) %>%
+    rename_all(~ c("id", "datet", "press", "temp", "depth")) %>%
     mutate(
       datet = mdy_hms(datet),
       SDATE = paste0(
@@ -37,7 +37,13 @@ tdr_proc <- function(x) {
         "-",
         month(datet, label = TRUE, abbr = TRUE),
         "-",
-        year(datet), " ", hour(datet), ":", minute(datet), ":", second(datet)
+        year(datet),
+        " ",
+        str_pad(hour(datet), width = 2, side = 'left', 0),
+        ":",
+        str_pad(minute(datet), 2, 'left', pad = 0),
+        ":",
+        str_pad(second(datet), 2, 'left', 0)
       ),
       YEAR = year(datet),
       MONTH = month(datet),
@@ -48,23 +54,24 @@ tdr_proc <- function(x) {
       TEMP = temp,
       DEPTH = press
     ) %>%
-    select(
-      STATION,
-      SDATE,
-      YEAR,
-      MONTH,
-      DAY,
-      HOUR,
-      MINUTE,
-      TEMP,
-      DEPTH
-    ) %>%
-    write_csv(paste0("R:/Science/CESD/COERS/FPage/data/TDR/data/for_oracle_import/",
-      last(.$YEAR),
-      "-",
-      str_pad(last(.$MONTH), 2, "left", "0"),
-      "-",
-      str_pad(last(.$DAY), 2, "left", "0"),
-      "_SABS_warf_water_temperature_data.csv"
-    ))
+    select(STATION,
+           SDATE,
+           YEAR,
+           MONTH,
+           DAY,
+           HOUR,
+           MINUTE,
+           TEMP,
+           DEPTH) %>%
+    write_csv(
+      paste0(
+        "R:/Science/CESD/COERS/FPage/data/TDR/data/for_oracle_import/",
+        last(.$YEAR),
+        "-",
+        str_pad(last(.$MONTH), 2, "left", "0"),
+        "-",
+        str_pad(last(.$DAY), 2, "left", "0"),
+        "_SABS_warf_water_temperature_data.csv"
+      )
+    )
 }
