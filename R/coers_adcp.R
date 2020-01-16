dce6e21bf1775cbd41899974beff0dd5bd8aad1c
Kraska, Peter <peter.kraska@dfo-mpo.gc.ca>
2020-01-16 10:07:42 -0400

    working more on COERS_ADCP

--- R/coers_adcp.R
+++ R/coers_adcp.R
@@ -1,8 +1,35 @@
-x <- "R:/Science/CESD/COERS/FPage/data/ADCP/Calders Head Salmon2019_1 20190521T180054.pd0"
-x <- "/home/kraskape/Documents/data/Calders Head Salmon2019_1 20190521T180054.pd0"
+x <-
+  "R:/Science/CESD/COERS/FPage/data/ADCP/Calders Head Salmon2019_1 20190521T180054.pd0"
+x <-
+  "/home/kraskape/Documents/data/Calders Head Salmon2019_1 20190521T180054.pd0"
 library(oce)
 library(data.table)
 library(tidyverse)
-coers_adcp <- function(x) {
-  adcp_raw <- read_csv("~/Documents/data/Calders Head Salmon2019_1 20190521T180054.averaged.txt")
-}
+
+raw <- fread("~/Documents/data/Calders Head Salmon2019_1 20190521T180054.averaged.txt", fill = TRUE, header = FALSE)
+
+variables <- unlist(raw[,8][1:9],use.names = FALSE)
+col_names <- unlist(raw[1], use.names = FALSE)
+
+data <- slice(raw, 10:n()) %>%
+  mutate(variable = rep(variables, length.out = n())) %>%
+  select(variable, everything())
+%>%
+  t()
+
+%>%
+  rename(.vars(col_names))
+
+= vars_select(num_range(prefix = "V", range = 1:9)))
+,
+         col_names[2] = V2,
+         col_names[3] = V3,
+         col_names[4] = V4,
+         col_names[5] = V5,
+         col_names[6] = V6,
+         col_names[7] = V7
+         )
+
+select(ensemble = Num, variable, everything())
+
+
